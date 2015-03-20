2ee0329400ed3b8ac43d4db814f42843708b29b2
Bruno Vilela de Moraes e Silva <brunovilelasilva@hotmail.com>
2015-03-20 17:23:10 +0100

    Changes in lets_maplizer and tests added

--- R/lets_maplizer.R
+++ R/lets_maplizer.R
@@ -1,19 +1,24 @@
-#' Create a matrix summarizing species' attributes within cells of a PresenceAbsence object
+#' Create a matrix summarizing species' attributes within 
+#' cells of a PresenceAbsence object
 #' 
 #' @author Bruno Vilela
 #' 
 #' @description Summarize species atributes per cell in a presence-absence matrix.
-#'
-#' @usage lets.maplizer(x, y, z, func=mean, ras=FALSE)
 #' 
-#' @param x A PresenceAbsence object.
-#' @param y Species attribute to be considered.
-#' @param z Species names in the same order as the attributes.
-#' @param func function to summarize the species' atribute.
-#' @param ras If \code{TRUE} the raster object will be returned together with the matrix
+#' @param x A \code{PresenceAbsence} object.
+#' @param y Species attribute to be considered. It must be a numeric attribute.
+#' @param z Species names in the same order as the attributes and exactly the 
+#' same as named in the \code{PresenceAbsence} object.
+#' @param func A function to summarize the species' atribute in each cell.
+#' @param ras If \code{TRUE} the raster object will be returned 
+#' together with the matrix.
 #' 
-#' @return Returns a matrix with the cells' geographic coordinates and the summarized species' attributes within them.
-#' 
+#' @return The result can be both a \code{matrix} or a \code{list} cointaining 
+#' the follow objects:
+#' @return \strong{Matrix}: a \code{matrix} object with the cells' geographic 
+#' coordinates and the summarized species' attributes within them.
+#' @return \strong{Raster}: The summarized species'attributed maped in a 
+#' \code{raster} object.
 #' 
 #' @seealso \code{\link{lets.presab}}
 #' @seealso \code{\link{lets.presab.birds}}
@@ -21,41 +26,53 @@
 #' 
 #' @examples \dontrun{
 #' data(PAM)
-#' trait <- runif(32)
-#' resu <- lets.maplizer(PAM, trait, PAM$S, ras=TRUE)
+#' data(IUCN)
+#' trait <- IUCN$Description_Year
+#' resu <- lets.maplizer(PAM, trait, PAM$S, ras = TRUE)
 #' head(resu$Matrix)
-#' plot(resu$raster) ; map(add=T)
+#' plot(resu$Raster, xlab = "Longitude", ylab = "Latitude", 
+#' main = "Mean description year per site") ; map(add = T)
 #' }
 #' 
 #' @export
 
-lets.maplizer <- function(x, y, z, func=mean, ras=FALSE){
-
-  if(is.factor(y)){
-  y <- as.numeric(levels(y))[y]
- }
-
- p <- x$P[, -(1:2)]
-
- for(i in 1:ncol(p)){
-  pos <- which(x$S[i]==z)
-  if(length(pos)>0){
-  p[, i] <- p[, i]*y[pos]
-  pos2 <- which(p[, i]==0)
-  p[pos2, i] <- NA
-  }else{
-    p[, i] <- NA
-  }  
- }
-
- resum <- apply(p, 1, func, na.rm=T)
- resultado <- cbind(x$P[, 1:2], resum)
- resu2 <- na.omit(resultado)
-
- if(ras==T){
-  r <- rasterize(resu2[, 1:2], x$R, resu2[, 3])
-  return(list(Matrix=resultado, raster=r))
- }else{
-  return(resultado)
- }
+lets.maplizer <- function(x, y, z, func = mean, ras = FALSE) {
+  
+  # Change factor to numbers
+  if (is.factor(y)) {
+    y <- as.numeric(levels(y))[y]
+  }
+  
+  # Get the matrix without coordinates
+  p <- x[[1]][, -(1:2)]
+  
+  for(i in 1:ncol(p)) {
+    pos <- x[[3]][i] == z
+    if (length(pos) > 0) {
+      p[, i] <- p[, i] * y[pos]
+      pos2 <- p[, i] == 0
+      p[pos2, i] <- NA
+      } else {
+        p[, i] <- NA
+      }
+  }
+  
+  resum <- apply(p, 1, func, na.rm=T)
+  
+  # Matrix of result 
+  resultado <- cbind(x[[1]][, 1:2], resum)
+  resu2 <- na.omit(resultado)
+  
+  # Name change
+  name <- paste("Variable", as.character(substitute(func)),
+                sep = "_")
+  colnames(resultado)[3] <- name 
+  
+  # Return result with or without the raster
+  if (ras) {
+    r <- rasterize(resu2[, 1:2], x[[2]], resu2[, 3])
+    return(list(Matrix = resultado, Raster = r))
+    } else {
+      return(resultado)
+    }
 }
