533e4b668289c8849d911a93ae0ab77663614ffe
Bruno Vilela de Moraes e Silva <brunovilelasilva@hotmail.com>
2015-03-24 15:00:32 +0100

    lets_iucn_ha changes and tests

--- R/lets_iucn_habitats.R
+++ R/lets_iucn_habitats.R
@@ -25,7 +25,8 @@
 #' lets.iucn.ha("Pongo pygmaeus")
 #' 
 #' # Multiple species
-#' sp <- c("Musonycteris harrisoni", "Ailuropoda melanoleuca", "Cebus flavius")
+#' sp <- c("Musonycteris harrisoni", "Ailuropoda melanoleuca",
+#'         "Cebus flavius")
 #' lets.iucn.ha(sp)
 #' }
 #' 
@@ -42,9 +43,6 @@ lets.iucn.ha <- function(input, count = FALSE) {
   sps <- input 
   
   
-  #create an empty matrix
-  habitat <- matrix(0, nrow = length(input), ncol = 19) 
-  
   #Habitat names (and the name "Species" that will be used in the matrix columns names)
   names <- c("Species", "Forest", "Savanna", "Shrubland", "Grassland", 
              "Wetlands", "Rocky areas", "Caves and Subterranean Habitats", 
@@ -52,83 +50,128 @@ lets.iucn.ha <- function(input, count = FALSE) {
              "Marine Intertidal", "Marine Coastal/Supratidal", "Artificial/Terrestrial", 
              "Artificial/Aquatic", "Introduced Vegetation", "Other", "Unknown")
   
+  #create an empty matrix
+  habitat <- matrix(0, nrow = length(input), ncol = length(names)) 
+  
   #Adding the column names
   colnames(habitat) <- names
   
   n <- length(input)
-
- if (count) {  
-  dev.new(width = 2, height = 2, pointsize = 12)
-  par(mar = c(0, 0, 0, 0))  
   
-  for(i in 1:n){
+  # With count window
+  if (count) {
     
-    plot.new()
-    text(0.5, 0.5, paste(paste("Total:", n, "\n", "Runs to go: ", (n - i))))      
-    
-    #Taking the Website code from the internet
-    input <- gsub(as.matrix(input), pattern = " ", replacement = "-")
-    h <- try(htmlParse(paste("http://api.iucnredlist.org/go/", input[i], sep = "")),
-             silent = TRUE)
-    b <- try(xpathSApply(h, '//div', xmlValue),
-             silent = TRUE)[1]
-    c <- as.numeric(gsub("\\D", "", b))
-    h2 <- try(htmlParse(paste("http://www.iucnredlist.org/details/classify/", 
-                              c, "/0", sep = "")),
-              silent=TRUE)
-    
-    #taking the specific parts that contain the habitat names
-    b2 <- try(xpathSApply(h2, '//html', xmlValue),
-              silent=TRUE)
-    
-    #look for the habitat names inside the string (if the sting contains the name, it will be marked 1)
-    for(t in 2:18) {
-      if (sum(grep(names[t], b2)) > 0) {
-        habitat[i, t] <- 1
-      }
+    # Do not set a new device in rstudio to avoid warnings()
+    if (!"tools:rstudio" %in% search()) {
+      dev.new(width = 2, height = 2, pointsize = 12)
+      par(mar = c(0, 0, 0, 0))
     }
     
-    #If none of the habitat names has been found or if the species has not been found in IUCN archives,
-    #it will have 1 in the column Unknwon
-    if (sum(habitat[i, ]) == 0) {
-      habitat[i, 19] <- 1
+    for(i in 1:n){
+      plot.new()
+      text(0.5, 0.5, paste(paste("Total:", n, "\n",
+                                 "Species to go: ",
+                                 (n - i))))
+      
+      ncolumns <- ncol(habitat)
+      habitat[i, 2:(ncolumns - 1)] <- .Habitat(input, i, 
+                                               habitat,
+                                               names)
+      
+      # If none of the habitat names has been found or if 
+      # the species has not been found in IUCN archives, 
+      # it will have 1 in the column Unknwon
+      if (sum(habitat[i, ]) == 0) {
+        habitat[i, ncolumns] <- 1
+      }
+    }
+    dev.off()
+  }
+  
+  if (!count) {
+    for(i in 1:n){
+      
+      ncolumns <- ncol(habitat)
+      habitat[i, 2:(ncolumns - 1)] <- .Habitat(input, i, 
+                                             habitat,
+                                             names)
+      
+      # If none of the habitat names has been found or if 
+      # the species has not been found in IUCN archives, 
+      # it will have 1 in the column Unknwon
+      if (sum(habitat[i, ]) == 0) {
+        habitat[i, ncolumns] <- 1
+      }
     }
   }
   
-  dev.off()
- }
-
- if (!count) {
-   for(i in 1:n){
-       
-    #Taking the Website code from the internet
-    input <- gsub(as.matrix(input), pattern=" ", replacement="-")
-    h <- try(htmlParse(paste("http://api.iucnredlist.org/go/", input[i], sep = "")), silent=TRUE)
-    b <- try(xpathSApply(h, '//div', xmlValue), silent=TRUE)[1]
-    c <- as.numeric(gsub("\\D", "", b))
-    h2 <- try(htmlParse(paste("http://www.iucnredlist.org/details/classify/", c, "/0", sep = "")), silent=TRUE)
-    
-    #taking the specific parts that contains the habitat names
-    b2 <- try(xpathSApply(h2, '//html', xmlValue), silent=TRUE)
-    
-    #look for the habitat names inside the string (if the sting contains the name, it will be marked 1)
-    for(t in 2:18){
-      if(sum(grep(names[t], b2))>0){
-        habitat[i, t] <- 1
-      }
-    }
-    
-    #If none of the habitat names has been found or if the species has not been found in the IUCN archives.
-    #it will have 1 in the column Unknwon
-    if (sum(habitat[i, ]) == 0) {
-      habitat[i, 19] <- 1
-    }
-  }  
- }
-
   #Putting species' names in the first column
   habitat[, 1] <- sps
   
   #Return the resulting matrix
   return(as.data.frame(habitat))
+}
+
+
+# Auxiliar funciton inside the loop
+.Habitat <- function(input, i, habitat, names) {
+  # Taking the Website code from the internet
+
+  c <- .getcode(input, i)
+  httpclas <- "http://www.iucnredlist.org/details/classify/"
+  h2 <- try(htmlParse(paste(httpclas, c, "/0", sep = "")),
+            silent = TRUE)
+  
+  # Taking the specific parts that contain the habitat names
+  b2 <- try(xpathSApply(h2, '//html', xmlValue), silent = TRUE)
+  
+  # Look for the habitat names inside the string 
+  # (if the sting contains the name, it will be marked 1)
+  Nnames <- length(names)
+  habitatParcial <- numeric(Nnames - 2)
+  
+  for(t in 2:(Nnames - 1)) {
+    if (sum(grep(names[t], b2)) > 0) {
+      habitatParcial[(t - 1)] <- 1
+    }
+  }
+  return(habitatParcial)
+}
+
+
+### Get the species code function
+.getcode <- function(input, i) {
+  input <- gsub(as.matrix(input), pattern = " ",
+                replacement = "-")
+  h <- try(htmlParse(paste("http://api.iucnredlist.org/go/",
+                           input[i], sep = "")),
+           silent = TRUE)
+  
+  b <- try(xpathSApply(h, '//div', xmlValue), silent = TRUE)[1]
+  c <- as.numeric(gsub("\\D", "", b))
+  
+  
+  # Subsecies control
+  http <- "http://www.iucnredlist.org/details/summary/"
+  h1 <- try(htmlParse(paste(http, c, "/0", sep = "")),
+            silent = TRUE)
+  
+  links <- xpathSApply(h1, "//a/@href")
+  links <- strsplit(links, "\n")
+  parents <- xpathSApply(h1, "//a")
+  
+  # function to transform xml in character list
+  tocharacter <- function(x) {
+    do.call(paste, as.list(capture.output(x)))
+  }
+  parents2 <- sapply(parents, tocharacter)
+  menos <- length(parents2)-length(links)
+  
+  posParent <- grep("_parent", parents2)
+  if (length(posParent) == 1) {
+    cpar <- gsub("\\D", "", (links[posParent - menos]))
+    c <- as.numeric(substr(cpar, 1, nchar(cpar)-1))
+  }
+  ################################################
+  return(c)
 }
