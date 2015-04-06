7ec67555175db870ad8ba514706749c7c18d7ac5
Fabricio Villalobos <fabricio.villalobos@gmail.com>
2015-04-06 12:41:45 -0300

    Update lets_presab_points.R
    change "shown" to "returned" when show.matrix=TRUE

--- R/lets_presab_points.R
+++ R/lets_presab_points.R
@@ -14,7 +14,7 @@
 #' @param resol Numeric vector of length 1 or 2 to set the grid resolution.
 #' @param remove.cells Logical, if \code{TRUE} the final matrix will not contain cells in the grid with a value of zero (i.e. sites with no species present).
 #' @param remove.sp Logical, if \code{TRUE} the final matrix will not contain species that do not match any cell in the grid.
-#' @param show.matrix Logical, if \code{TRUE} only the presence-absence matrix will be shown.
+#' @param show.matrix Logical, if \code{TRUE} only the presence-absence matrix will be returned.
 #' @param crs Character representign the PROJ.4 type description of a Coordinate Reference System (map projection).
 #' @param count Logical, if \code{TRUE} a counting window will open.
 #' 
